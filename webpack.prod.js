1c07ba71c7cac234654e1cd74af6a50484203324
Layinka Jaji <layinkawebdev@gmail.com>
2021-10-03 13:11:48 +0100

    Webpack setup

---
+++ webpack.prod.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: "./src/client/index.js",
+    mode: "production",
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: "var",
+        library: "Travel",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCSSExtractPlugin.loader, "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCSSExtractPlugin({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW()
+    ]
+}
