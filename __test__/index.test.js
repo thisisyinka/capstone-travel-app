4d71950bafa79c0cee7ab11f1c963057cb4b1c65
Layinka Jaji <layinkawebdev@gmail.com>
2021-10-14 17:13:46 +0100

    Fix: change require to import in test files

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,4 +1,4 @@
-const getTravelData = require('../src/client/js/index');
+import { getTravelData } from "../src/client/js/index";
 
 describe("Testing getting travel data functionality", () => {
     test("Testing the getTravelData function and validate date input as a string", () => {
